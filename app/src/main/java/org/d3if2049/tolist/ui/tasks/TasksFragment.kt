750150f71c02c9f213a4d40fb2c09b96ad4fb957
MuhammadZulfikarIkhsan <muhzulfikarikhsan@student.telkomuniversity.ac.id>
2022-05-22 19:54:15 +0700

    ListAdapter with View Binding

--- app/src/main/java/org/d3if2049/tolist/ui/tasks/TasksFragment.kt
+++ app/src/main/java/org/d3if2049/tolist/ui/tasks/TasksFragment.kt
@@ -1,12 +1,36 @@
 package org.d3if2049.tolist.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import org.d3if2049.tolist.R
+import org.d3if2049.tolist.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTugas.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/org/d3if2049/tolist/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/org/d3if2049/tolist/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package org.d3if2049.tolist.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import org.d3if2049.tolist.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
