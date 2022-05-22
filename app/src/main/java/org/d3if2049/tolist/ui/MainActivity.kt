05e24098a1f3ca9b38694f807e059598f7136e91
MuhammadZulfikarIkhsan <muhzulfikarikhsan@student.telkomuniversity.ac.id>
2022-05-22 17:35:09 +0700

    Navigation Component

--- app/src/main/java/org/d3if2049/tolist/ui/MainActivity.kt
+++ app/src/main/java/org/d3if2049/tolist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.d3if2049.tolist
+package org.d3if2049.tolist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.d3if2049.tolist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/org/d3if2049/tolist/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package org.d3if2049.tolist.ui.tasks
+
+import androidx.fragment.app.Fragment
+import org.d3if2049.tolist.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
