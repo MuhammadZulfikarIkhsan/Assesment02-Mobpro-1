46f45fbf4be6acf9bbfb0a4c8b8c40355a86c234
MuhammadZulfikarIkhsan <muhzulfikarikhsan@student.telkomuniversity.ac.id>
2022-05-22 17:17:22 +0700

    Layouts and Room Entity

---
+++ app/src/main/java/org/d3if2049/tolist/data/Task.kt
@@ -0,0 +1,23 @@
+package org.d3if2049.tolist.data
+
+import android.icu.text.DateFormat
+import android.os.Build
+import android.os.Parcelable
+import androidx.annotation.RequiresApi
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "tabel_tugas")
+@Parcelize
+data class Task(
+    val name: String,
+    val important: Boolean = false,
+    val completed: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+) : Parcelable {
+    val createdDateFormatted: String
+        @RequiresApi(Build.VERSION_CODES.N)
+        get() = DateFormat.getDateTimeInstance().format(created)
+}
