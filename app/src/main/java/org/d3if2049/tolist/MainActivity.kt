02e3e42d7852f63667f4d8e797961859f8523357
MuhammadZulfikarIkhsan <muhzulfikarikhsan@student.telkomuniversity.ac.id>
2022-05-22 11:38:22 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if2049/tolist/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if2049.tolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
